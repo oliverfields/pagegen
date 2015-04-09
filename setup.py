63ac839a1ab645616257e36e2bf71b59d0fdd164
Oliver Fields <oliver@phnd.net>
2015-04-09 13:32:05 +0200

    Major reshuffle of files for python packaging, no new functionality

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+files=['skel/*', 'pagegen.conf', 'changelog']
+
+setup(name = 'pagegen',
+	version='2.0.4',
+	description='Static site generator',
+	author='Oliver Fields',
+	author_email='pagegen@phnd.net",
+	url='http://pagegen.phnd.net',
+	license='GPLv3',
+	packages=['pg'],
+	package_data={'pg' : files },
+	scripts=['pagegen'],
+	long_description="""Python static site generator with reStructuredText markup.""",
+	install_requires=['lxml','docutils']
+)
