97f4186ba84a45ade7f4cc74250b371dfcbc2456
Oliver Fields <oliver@phnd.net>
2021-12-09 13:05:37 +0100

    Pip setup changes

--- setup.py
+++ setup.py
@@ -6,12 +6,14 @@ setup(name = 'pagegen',
 	version='2.1.1',
 	description='Static site generator',
 	author='Oliver Fields',
-	author_email='pagegen@phnd.netr',
+	author_email='pagegen@phnd.net',
 	url='http://pagegen.phnd.net',
 	license='GPLv3',
-	packages=['pg'],
-	package_data={'pg' : files },
-	scripts=['pagegen'],
+	packages=['pagegen'],
+	package_data={'pagegen' : files },
 	long_description="""Python static site generator with reStructuredText markup.""",
-	install_requires=['lxml','docutils']
+	install_requires=['lxml','docutils','ConfigParser'],
+        entry_points={
+            'console_scripts': ['pagegen=pagegen.pagegen_command:main']
+        }
 )
