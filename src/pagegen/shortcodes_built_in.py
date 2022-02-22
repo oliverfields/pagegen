e6c00dc72dcf920d10f0138c672f1a097acf1912
Oliver Fields <oliver@phnd.net>
2022-02-22 18:41:37 +0000

    Updated image shortcode

--- src/pagegen/shortcodes_built_in.py
+++ src/pagegen/shortcodes_built_in.py
@@ -1,5 +1,6 @@
 from pagegen.utility import DIRDEFAULTFILE, appropriate_markup, generate_menu, CONTENTDIR, ASSETDIR
 from os.path import isfile, getctime
+from os import makedirs
 from PIL import Image, ImageOps
 
 
@@ -42,54 +43,84 @@ def built_in_menu(site, page):
 	return page.menu
 
 
-def built_in_image(site, page, image_source_relative, alt_attribute, image_class=None, image_size=None):
+def built_in_image(site, page, image_source, alt_attribute, target_dir=None, image_class=None, image_size=None):
 	''' Return img tag and optionally resize image using image_class settings from site.conf or image_size(XxY) argument. Maintains aspect ration by cropping with center gravity and resize to dimensions '''
 
-	# Relative paths are relative to site_dir
-	asset_dir = site.site_dir + '/' + CONTENTDIR + '/' + ASSETDIR + '/'
-	if image_source_relative.startswith('/') or image_source_relative.startswith('.'):
-		raise Exception('Image source path must be relative to ' + site.site_dir + ', must not start with "/" or ".": ' + image_source_relative)
+	resize = False
 
-	image_source_full = asset_dir + image_source_relative
+	# Path cannot start with .
+	if image_source.startswith('.'):
+		raise Exception('Image source cannot start with .')
 
-	if not isfile(image_source_full):
-		raise Exception('Cannot find ' + image_source_full)
+	# If path is relative, prefix with site_dir
+	if not image_source.startswith('/'):
+		image_source = site.site_dir + '/' + image_source
+
+	# Check source image exists
+	if not isfile(image_source):
+		raise Exception('No file: ' + source_image)
+
+	t = image_source.rpartition('/')
+	source_dir = t[0]
+	source_file = t[2]
+
+	# Target dir defults to assets
+	if target_dir is None:
+		target_dir = site.asset_dir 
+	else:
+		# Target dir cannot start with .
+		if target_dir.startswith('.'):
+			raise Exception('target_dir cannot start with .')
+
+		# Target_dir is always relative to asset dir
+		if target_dir.startswith('/'):
+			target_dir = site.asset_dir + target_dir
+		else:
+			target_dir = site.asset_dir + '/' + target_dir
 
 	# Lookup class settings if image_class argument
 	if image_class:
-		image_file_name_part = image_class
 		width = site.image_classes[image_class]['width']
 		height = site.image_classes[image_class]['height']
+		target_file = source_file.replace('.', '-' + image_class + '.')
+		resize = True
 	elif image_size:
-		image_file_name_part = image_size
 		dims = image_size.split('x')
+		target_file = source_file.replace('.', '-' + image_size + '.')
 
 		if len(dims) != 2:
 			raise Exception('Unable to parse WidthxHeight: ' + image_size)
 
 		width = int(dims[0])
 		height = int(dims[1])
+		resize = True
 	else:
-		raise Exception('Must call with either image_class or image_size arguments')
+		target_file = source_file
+		resize = False
 
-	image_target_relative = image_source_relative.replace('.', '-' + image_file_name_part + '.')
-	image_target_full = site.content_dir + '/' + ASSETDIR + '/' + image_target_relative
+	# Image src
+	length_content_dir_path = len(site.site_dir + '/' + CONTENTDIR)
+	img_src = site.base_url + target_dir[length_content_dir_path:] + '/' + target_file
 
-	img_src = site.base_url + '/' + ASSETDIR + '/' + image_target_relative
+	# Create target dir if needed
+	makedirs(target_dir, exist_ok=True)
+
+	target_file_path = target_dir + '/' + target_file
 
 	#If image already exists, check if source is newer than target, else is target has correct dimensions, failing all that then resize
 	create_target = False
-	if isfile(image_target_full):
-		if getctime(image_target_full) < getctime(image_source_full):
+	if isfile(target_file_path):
+		if getctime(target_file_path) < getctime(image_source):
 			create_target = True
 	else:
 		create_target = True
 
 	if create_target:
-		im = Image.open(image_source_full)	
+		im = Image.open(image_source)	
 		im_new = ImageOps.fit(im, (width, height))
-		im_new.save(image_target_full)
+		im_new.save(target_file_path)
 
 	html = '<img src="' + img_src + '" alt="' + alt_attribute + '" width="' + str(width) + '" height="' + str(height) + '" />'
 
 	return html
+
