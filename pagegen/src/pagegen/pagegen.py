91ea00cb2a15c2e4665ccdb214ed42076cf1f683
Oliver Fields <oliver@phnd.net>
2025-01-17 18:23:11 +0100

    tweak

--- pagegen/src/pagegen/pagegen.py
+++ pagegen/src/pagegen/pagegen.py
@@ -1,15 +1,12 @@
 import argparse
 from sys import exit
-from os import system
+from os import system, open, O_CREAT, O_EXCL, remove
+from os.path import join, isfile
 from Common import Common
 from traceback import print_exception
-from constants import SITE_ENV
+from constants import SITE_ENV, LOCK_FILE
 from Site import Site
 from pathlib import Path
-
-
-def generate_site():
-    print('hi')
 
 
 def find_site_dir(path=False):
@@ -54,9 +51,24 @@ if __name__ == '__main__':
                 c.log_error(f'Unable to find {SITE_ENV}')
                 exit(1)
 
+            lock_path = join(site_dir, LOCK_FILE)
+
+            if isfile(lock_path):
+                c.log_error(f'Lock file found, try again or delete: {lock_path}')
+                exit(1)
+            else:
+                open(lock_path, O_CREAT | O_EXCL)
+
+            import time
+            time.sleep(20)
             s = Site(site_dir=site_dir, settings=settings)
 
+        remove(lock_path)
 
+    except KeyboardInterrupt:
+        remove(lock_path)
+        print('')
+        exit(1)
     except Exception as e:
         print('cache could be inconsistent, delete it?')
         c.log_error('Unknown failure')
