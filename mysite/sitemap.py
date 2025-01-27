4db07fb20421013946a9ac8ac27412f0efb830e9
Oliver Fields <oliver@phnd.net>
2025-01-27 15:46:51 +0100

    tweak

--- mysite/sitemap.py
+++
@@ -1,8 +0,0 @@
-class Plugin():
-
-    def __init__(self):
-        print('Loading myself today then')
-
-
-    def pgn_hook_pre_build(self):
-        print('helloe from a plugin sitemap is my name')
