e4bf9f582c56484dfeb08b2b6d25d7d031b217cc
Oliver Fields <oliver@phnd.net>
2025-02-02 18:16:19 +0100

    tweak

---
+++ psite/plugins/sitemenu/conf.py
@@ -0,0 +1,38 @@
+conf = {
+    "menu": {
+        "index": "Home",
+        "manual": {
+            "manual/content-management": {
+                "manual/content/management/executable-files": "Exacutable files",
+                "manual/content/management/page-markup": "Page markup",
+                "manual/content/management/page-headers-and-content": "Page headers and content",
+                "manual/content/management/naming-conventions": "Naming conventions",
+                "manual/content/management/content-directory": "Content directory"
+             },
+            "manual/site-and-page-attributes": "Site and page attributes",
+            "manual/installation": "Installation",
+            "manual/site-generation": "Site generation",
+            "manual/blogging": "Blogging",
+            "manual/design-and-layout": {
+                "manual/design-and-layout/assets": "Assets",
+                "manual/design-and-layout/site-menu": "Site menu",
+                "manual/design-and-layout/templates-and-themes": "Templates and themes"
+            },
+            "manual/shell": "Shell",
+            "manual/shortcodes": "Shortcodes",
+            "manual/hooks": "Hooks",
+        },
+        "articles": {
+            "articlse/web-server-tips": "Web server tips",
+            "articles/pipleline-with-github-actions": "Pipeline with Github Actions"
+        },
+        "quick-start": "Quick start"
+    },
+    "ignore": [
+        "404",
+        "search-results",
+        "robots.txt",
+        "BingSiteAuth.xml",
+        ".htaccess"
+    ]
+}
---
+++ psite/plugins/sitemenu/sitemenu.py
@@ -0,0 +1,17 @@
+from conf import conf
+
+def to_html(d, c = 0):
+   for a, b in d.items():
+      if isinstance(b, str):
+          yield '{}<li><a href="{}">{}</a></li>'.format('   '*c, a, b)
+      elif isinstance(b, dict):
+          yield '{}<li class="toggle-expand">{} -'.format('   '*c, a)
+
+      if isinstance(b, dict):
+         yield '{}<ul>\n{}\n{}</ul></li>'.format('   '*c, "\n".join(to_html(b, c + 1)), '   '*c)
+
+print('<ul>' + '\n'.join(to_html(conf['menu'])) + '</ul>')
+
+
+#import pprint
+#pprint.pprint(conf)
