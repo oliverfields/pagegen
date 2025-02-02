9a98ce66b53cdd3be14ccbc4a9fb5a0a4ff4128a
Oliver Fields <oliver@phnd.net>
2025-02-02 20:53:11 +0100

    tweak

--- psite/plugins/sitemenu/sitemenu.py
+++ psite/plugins/sitemenu/sitemenu.py
@@ -1,16 +1,66 @@
 from conf import conf
 
-def to_html(d, c = 0):
+def to_html(d):
     for a, b in d.items():
         if isinstance(b, str):
             print('check file exists')
-            yield '{}<li><a href="{}">{}</a></li>'.format('   '*c, a, b)
+            yield '<li><a href="{}">{}</a></li>'.format(a, b)
         elif isinstance(b, dict):
-            yield '{}<li class="toggle-expand">{} -'.format('   '*c, a)
-            yield '{}<ul>\n{}\n{}</ul></li>'.format('   '*c, "\n".join(to_html(b, c + 1)), '   '*c)
+            yield '<li><span class="toggle-expand">{} <span>▼</span></span>\n<ol>\n{}</ol>\n</li>'.format(a, "\n".join(to_html(b)))
 
-print('<ul>' + '\n'.join(to_html(conf['menu'])) + '</ul>')
+print(''' <html>
+<head>
+<style>
+#site-menu ol {
+display: none;
+}
+</style>
+</head>
+<body>
+''')
 
+print('<ol id="site-menu">' + '\n'.join(to_html(conf['menu'])) + '</ol>')
 
-#import pprint
-#pprint.pprint(conf)
+print('''
+<script>
+function show_current_page_parent_menus(obj){
+  if (typeof obj === 'undefined') return;
+  if (obj.id == 'site-menu') return;
+  if (obj.parentElement.tagName == 'OL') {
+    obj.parentElement.style.display='block';
+  }
+  show_current_page_parent_menus(obj.parentElement);
+}
+
+let m = document.getElementById('site-menu');
+
+// Add click event to toggle sub menus
+let showHideLinks = m.getElementsByClassName('toggle-expand');
+for (let i = 0; i < showHideLinks.length; i++) {
+  showHideLinks[i].addEventListener("click", function() {
+    for (const child of this.parentElement.children) {
+      if (child.tagName == 'OL') {
+        if (child.style.display == 'block') {
+          this.children[0].innerHTML = '▼';
+          child.style.display = 'none';
+        }
+        else {
+          this.children[0].innerHTML = '▲';
+          child.style.display = 'block';
+        }
+      }
+    }
+  });
+}
+
+// Show current page
+page_url = '/articles/web-server-tips';
+let menuLinks = m.getElementsByTagName('A');
+for (let i = 0; i < menuLinks.length; i++) {
+  if (menuLinks[i].href == 'file://' + page_url) {
+    show_current_page_parent_menus(menuLinks[i]);
+  }
+}
+</script>
+</body>
+</html>''')
