4b501364a22d39fd0560dc3c8882baeb02fdb1a9
Oliver Fields <oliver@phnd.net>
2025-02-03 20:57:54 +0100

    tweak

--- pagegen/src/pagegen/plugins/tags/tags.py
+++ pagegen/src/pagegen/plugins/tags/tags.py
@@ -1,13 +1,32 @@
-class Plugin():
+import pagegen.logger_setup
+import logging
 
-    def hook_pre_build(self, objects):
-        print('pgn pre building tags dude')
+logger = logging.getLogger('pagegen.' + __name__)
+
+class Plugin():
 
     def hook_post_build(self, objects):
         i = objects['index']
+        tags = {}
 
-        for url, p_meta in objects['index'].items():
+        for url, p_meta in i.items():
             try:
-                print(url + ': ' + p_meta['headers']['tags'])
+                p_tags_csv = p_meta['headers']['tags']
             except KeyError:
-                pass
+                continue
+
+            p_tags = p_tags_csv.split(',')
+
+            for t in p_tags:
+                if not t in tags.keys():
+                    tags[t] = []
+
+                try:
+                    tags[t].append({
+                        'url': p_meta['url'],
+                        'title': p_meta['headers']['title']
+                    })
+                except AttributeError as e:
+                    logger.error(f'Page missing {e.name} attribute: {url}')
+
+        print(tags)
