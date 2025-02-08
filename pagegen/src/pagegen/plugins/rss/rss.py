1564b59044423f0bd8290c205fee02ed8bd43336
Oliver Fields <oliver@phnd.net>
2025-02-08 10:29:38 +0100

    tweak

--- pagegen/src/pagegen/plugins/rss/rss.py
+++ pagegen/src/pagegen/plugins/rss/rss.py
@@ -1,5 +1,5 @@
 from pagegen.Common import Common
-from os.path import join
+from os.path import join, exists
 import pagegen.logger_setup
 import logging
 
@@ -18,12 +18,12 @@ class Plugin(Common):
         '''
 
         s = objects['site']
-        i = objects['index']
+        i = s.index
 
         feed_pages = []
         for p_path, p in i.items():
-            if not 'rss exclude' in p['headers'].keys() or not p['headers']['rss exclude']:
-                if not 'date' in p['headers'].keys():
+            if not 'rss' in p.headers.keys() or p.headers['rss'] != False:
+                if not 'date' in p.headers.keys():
                     logger.error(f'Page is missing mandatory date header: {p_path}')
                     raise Exception(f'Page is missing mandatory date header: {p_path}')
                 feed_pages.append(p)
@@ -42,12 +42,12 @@ class Plugin(Common):
 
 
         # Feed items
-        for p in sorted(feed_pages, key=lambda p: p['headers']['date']):
+        for p in sorted(feed_pages, key=lambda p: p.headers['date']):
             feed += '<item>\n'
             try:
-                feed += f'<title>{p["headers"]["title"]}</title>\n'
-                feed += f'<link>{p["url"]}</link>\n'
-                feed += f'<description>{p["headers"]["description"]}</description>\n'
+                feed += f'<title>{p.headers["title"]}</title>\n'
+                feed += f'<link>{p.absolute_url}</link>\n'
+                feed += f'<description>{p.headers["description"]}</description>\n'
 
             except KeyError as e:
                 logger.error(f'Page is missing mandatory {e.args[0]}: {p_path}')
@@ -57,6 +57,12 @@ class Plugin(Common):
 
         feed = '<?xml version="1.0" encoding="UTF-8" ?>\n<rss version="2.0">\n<channel>\n' + feed + '</channel>'
 
-        logger.info('Writing feed.rss')
-        self.write_file(join(s.build_dir, 'feed.rss'), feed)
+        rss_path = join(s.build_dir, 'feed.rss')
+
+        if not exists(rss_path):
+            logger.info(f'Writing RSS: {rss_path}')
+            self.write_file(rss_path, feed)
+        else:
+            logger.warning(f'RSS already exists: {rss_path}')
+
 
