654d5cfaee3f072acc56492c747a72232ecfe98c
Oliver Fields <oliver@phnd.net>
2025-02-20 15:36:16 +0100

    tweak

--- src/pagegen/plugins/site_search/site_search.py
+++ src/pagegen/plugins/site_search/site_search.py
@@ -24,7 +24,6 @@ class Plugin(Common):
         '''
 
         self.cache_dir = join(objects['site'].site_dir, CACHE_DIR, 'site_search')
-        self.terms = {} # Contains terms as key and Hit objects as values
         self.content_tags=['p','li', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'td', 'th', 'strong', 'em', 'i', 'b', 'a', 'blockquote', 'div', 'span', 'pre', 'abbr', 'address', 'cite', 'code', 'del', 'dfn', 'ins', 'kbd', 'q', 'samp', 'small', 'sub', 'sup', 'var', 'dt', 'dd', 'legend', 'caption', 'article', 'aside', 'details', 'figcaption', 'section', 'summary', 'title'] # HTML tags that may contain searchable content
         self.meta_tags = ['description'] # Meta tags attributes that may contain searchable content
 
@@ -66,6 +65,9 @@ class Plugin(Common):
         if 'search' in p.headers.keys() and p.headers['search'] == False:
             return
 
+        self.search_index[p.relative_url] = {}
+        si = self.search_index[p.relative_url]
+
         tree = etree.HTML(p.out)
 
         # Loop over meta tags
@@ -73,7 +75,7 @@ class Plugin(Common):
             try:
                 result = tree.xpath("//meta[@name='"+search_attribute+"']")
                 for tag in result:
-                    self.index_string(tag.attrib['content'], p.absolute_url, 5, p.headers['title'], p.headers['description'])
+                    self.index_string(si, tag.attrib['content'], 5)
             except:
                 raise
                 pass # No meta tags found, fair enough
@@ -84,11 +86,11 @@ class Plugin(Common):
             for tag in result:
                 text = ''
                 if tag.text:
-                    text += tag.text+' '
+                    text += tag.text + ' '
                 if tag.tail:
                     text += tag.tail
 
-                self.index_string(text, p.absolute_url, 10, p.headers['title'], p.headers['description'])
+                self.index_string(si, text, 5)
         except:
             pass # Unable to find a title
 
@@ -123,9 +125,7 @@ class Plugin(Common):
                         else:
                             weight=0
 
-                        self.index_string(text, p.absolute_url, weight, p.headers['title'], p.headers['description'])
-
-        print(self.terms)
+                        self.index_string(si, text, weight)
 
 
     def hook_post_build(self, objects):
@@ -137,14 +137,14 @@ class Plugin(Common):
         self.pickle_object(self.cache_dir, self.index_cache_file_name, self.search_index)
 
 
-    def index_string(self, string, url, weight, title, description):
+    def index_string(self, terms, string, weight):
         '''
-        For each word in string add to index if not a stop word
+        For each word in string add to terms with given weight, if not a stop word
         '''
 
         # Index only lowercase
-        string_original=string
-        string=string.lower()
+        string_original = string
+        string = string.lower()
 
         if not string:
             return True
@@ -153,56 +153,25 @@ class Plugin(Common):
 
         for word in string.split(' '):
 
-            word=sub('[^a-z0-9]*', '', word)
-            word=word.strip()
+            word = sub('[^a-z0-9]*', '', word)
+            word = word.strip()
 
             if len(word) == 0:
                 continue
 
             if word in self.stop_words:
-                #print 'Word "%s" is stop word for %s' % (word, hit.url)
+                #print('Word "%s" is stop word' % (word))
                 continue
 
-            hit=Hit(word, url, weight, string_original, title, description)
+            term_weight = weight
 
-            if word in self.terms.keys():
-                #print 'Considering "%s"..' % word
-                # Return if hit for this file alredy added
-                if not self.is_hit_indexed(hit):
-                    #print 'Adding "%s" to index for %s (%s)' % (repr(word), hit.url, weight)
-                    hits = self.terms[word]
-                    hits.append(hit)
-                    self.terms[word] = hits
-                # Update weight if is indexed
-                else:
-                    for indexed_hit in self.terms[word]:
-                        if indexed_hit.term == hit.term and indexed_hit.url == hit.url and indexed_hit.weight < hit.weight:
-                            #print "Increasing weight for hit '%s'" % hit.url
-                            indexed_hit.weight = hit.weight
-
+            # Update weight if is indexed
+            if word in terms.keys():
+                if terms[word] < term_weight:
+                    #print("Increasing weight for term '%s'" % word)
+                    terms[word] = term_weight
             else:
-                #print 'Adding "%s" to index for %s (%s)' % (repr(word), hit.url, weight)
-                self.terms[word]=[hit]
+                #print('Adding term "%s" to index (weight %s)' % (word, term_weight))
+                terms[word] = term_weight
 
-        return True
-
-
-    def is_hit_indexed(self, hit):
-        ''' Check if hit is indexed for given word and url '''
-        for indexed_hit in self.terms[hit.term]:
-            if hit.term==indexed_hit.term and hit.url==indexed_hit.url:
-                return True
-        return False
-
-
-class Hit:
-    ''' Weighted occurence of a term in file'''
-
-    def __init__(self, term, url, weight, line, title, description):
-        self.term=term
-        self.url=url
-        self.weight=weight
-        self.line=line
-        self.description=description.replace('"', '\"')
-        self.description=description.replace('\n', ' ')
-        self.title=title
+        return terms
